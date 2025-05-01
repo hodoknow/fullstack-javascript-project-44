528361a1e337900710a889cc696a4e2167241ac9
Solodukha <1hodok@gmail.com>
2025-04-30 20:30:53 -0400

    add sonarQube, prettier and eslint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import { greeting } from "../src/cli.js";
 
-const msg = 'Welcome to the Brain Games!';
+const msg = "Welcome to the Brain Games!";
 console.log(msg);
 
 greeting();
