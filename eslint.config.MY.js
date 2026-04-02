e0493f4582d3854ded35009ff9dc3a7950bc2194
Solodukha <1hodok@gmail.com>
2026-04-02 08:33:04 -0400

    add eslint config from hexlet

---
+++ eslint.config.MY.js
@@ -0,0 +1,41 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import stylistic from '@stylistic/eslint-plugin';
+
+export default defineConfig([
+  {
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    rules: {
+      // Кавычки — разрешены любые (ошибки не будет)
+      '@stylistic/quotes': 'off',
+      
+      // Точки с запятой — всегда
+      '@stylistic/semi': ['error', 'always'],
+      
+      // Отступы — табуляция, но пробелы только предупреждение (или 'off' для полного отключения)
+      // '@stylistic/indent': ['warn', 'tab'],
+      '@stylistic/indent': 'off',
+      
+      // Разрешённые ++ и --
+      'no-plusplus': 'off',
+      'no-minusminus': 'off',
+      
+      // Висячие запятые — только в многострочных
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
+    },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  { 
+    files: ['**/*.{js,mjs,cjs}'], 
+    languageOptions: { 
+      globals: globals.node,
+    },
+  },
+]);
--- eslint.config.js
+++ eslint.config.js
@@ -1,41 +1,14 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import { defineConfig } from 'eslint/config';
-import stylistic from '@stylistic/eslint-plugin';
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([
-  {
-    plugins: {
-      '@stylistic': stylistic,
-    },
-    rules: {
-      // Кавычки — разрешены любые (ошибки не будет)
-      '@stylistic/quotes': 'off',
-      
-      // Точки с запятой — всегда
-      '@stylistic/semi': ['error', 'always'],
-      
-      // Отступы — табуляция, но пробелы только предупреждение (или 'off' для полного отключения)
-      // '@stylistic/indent': ['warn', 'tab'],
-      '@stylistic/indent': 'off',
-      
-      // Разрешённые ++ и --
-      'no-plusplus': 'off',
-      'no-minusminus': 'off',
-      
-      // Висячие запятые — только в многострочных
-      '@stylistic/comma-dangle': ['error', 'always-multiline'],
-    },
-  },
+  stylistic.configs.recommended,
   {
     files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
     extends: ['js/recommended'],
+    languageOptions: { globals: globals.node },
   },
-  { 
-    files: ['**/*.{js,mjs,cjs}'], 
-    languageOptions: { 
-      globals: globals.node,
-    },
-  },
-]);
+])
